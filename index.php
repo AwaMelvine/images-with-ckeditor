a8c3ff6b393a4a2313017ba51cfa08435ef7786d
Awa <melvineawa@gmail.com>
2018-03-03 01:23:09 +0100

    Initial commit. Set up Bootstrap CSS & Js, and JQuey

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Uploading images in CKEditor using PHP</title>
+
+	<!-- Bootstra -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+<body>
+	
+	<div class="container">
+		<div class="row">
+			<button class="btn btn-primary btn-lg">Test</button>
+		</div>
+	</div>
+
+<!-- JQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- JQuery scripts -->
+<script>
+	$(document).ready(function(){
+		alert('works');
+	});
+</script>
+
+</body>
+</html>
