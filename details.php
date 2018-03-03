8722c9677c925b22591fc795abd8ee1330a48115
Awa <melvineawa@gmail.com>
2018-03-03 13:15:31 +0100

    Created mockup for the project.

---
+++ details.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Uploading images in CKEditor using PHP</title>
+
+	<!-- Bootstra -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+
+	<!-- Custom styling -->
+	<link rel="stylesheet" href="main.css">
+
+</head>
+<body>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="post-details">
+					<h3><a href="#">This is the very first post here</a></h3>
+					<p>Here goes some sample text that describes the situation hereHere goes some sample text that describes the situation hereHere goes some sample text that describes the situation here</p>
+				</div>				
+			</div>
+
+		</div>
+	</div>
+
+
+<!-- JQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- JQuery scripts -->
+<script>
+
+</script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,14 +6,54 @@
 
 	<!-- Bootstra -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+
+	<!-- Custom styling -->
+	<link rel="stylesheet" href="main.css">
+
 </head>
 <body>
 	
 	<div class="container">
 		<div class="row">
-			<button class="btn btn-primary btn-lg">Test</button>
+			<div class="col-md-6 col-md-offset-3 post-div">
+
+			<div class="post">
+				<h3><a href="details.php">This is the very first post here</a></h3>
+				<p>Here goes some sample text that describes the situation here</p>
+			</div>				
+			<div class="post">
+				<h3><a href="details.php">This is the very first post here</a></h3>
+				<p>Here goes some sample text that describes the situation here</p>
+			</div>	
+
+
+			<form action="index.php" class="post-form">
+				
+				<h1 class="text-center">Add Blog Post</h1>
+
+				<div class="form-group">
+				<label for="title">Title</label>
+					<input type="text" name="title" class="form-control" >
+				</div>
+
+				<div class="form-group">
+					<label for="post">Body</label>
+					<textarea name="body" id="body" class="form-control" cols="30" rows="5"></textarea>
+ 				</div>
+ 				<div class="form-group">
+ 					<button type="submit" class="btn btn-success pull-right">Save Post</button>
+ 				</div>
+
+			</form>
+
+				
+			</div>
+
 		</div>
 	</div>
+
+
+
 
 <!-- JQuery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -23,9 +63,7 @@
 
 <!-- JQuery scripts -->
 <script>
-	$(document).ready(function(){
-		alert('works');
-	});
+
 </script>
 
 </body>
